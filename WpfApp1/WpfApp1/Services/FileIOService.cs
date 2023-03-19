2f5393d89c30967deb33f04b767e17e0b6e02107
Dash <dasha.semivolos@gmail.com>
2023-03-19 22:22:16 +0300

    second project

---
+++ WpfApp1/WpfApp1/Services/FileIOService.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Models;
+
+namespace WpfApp1.Services
+{
+    internal class FileIOService
+    {
+        private readonly string PATH;
+
+        public FileIOService(string path)
+        {
+            PATH = path;
+        }
+
+        public BindingList<BudgetModel> LoadData()
+        {
+            var fileExists = File.Exists(PATH);
+            if (!fileExists)
+            {
+                File.CreateText(PATH).Dispose();
+                return new BindingList<BudgetModel>();
+            }
+            using (var reader = File.OpenText(PATH))
+            {
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<BudgetModel>>(fileText);
+            }
+        }
+
+        public void SaveData(object financeDataList) 
+        {
+            using (StreamWriter writer = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(financeDataList);
+                writer.WriteLine(output);
+            }
+        }
+    }
+}
